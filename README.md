145c06f9f1f1850bc7db9b3319ef65837c2cb748
kimjuno97 <jh241093@gmail.com>
2023-01-04 21:03:28 +0900

    완성본 추가

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# [WebRTC API를 사용한 화상 채팅 구현](https://kimjunho97.tistory.com/6)
+
+해당 Repository blog에 설명 글이 있습니다.
+
 ## 초기 세팅 폴더
 
 - 공통
@@ -6,3 +10,6 @@
 - backend
   nodemon이 없을 수도 있어서 devDependencies에 nodemon을 설치 하였습니다.
   에러가 뜨면 해당 부분을 확인하면 됩니다.
+
+- front
+  귀찮은 마크업은 넣어 두었습니다.
--- 초기세팅/front/main.js
+++ 초기세팅/front/main.js
@@ -1,4 +1,19 @@
 import { io } from 'socket.io-client';
+/** 본인 비디오 */
+const myFace = document.querySelector('.myFace');
+/** 카메라 on/off 버튼 */
+const cameraBtn = document.querySelector('.camera');
+/** 스피커 on/off 버튼 */
+const speakerBtn = document.querySelector('.speaker');
+/** 상대방 비디오  */
+const peerFace = document.querySelector('.peerFace');
+/** 본인 MediaStream 저장 */
+let myStream;
+/** speaker */
+let muted = false;
+/** video */
+let cameraOff = false;
+/** websocket으로 통신 */
 const socket = io('ws://localhost:3000', {
 	withCredentials: true,
 });
